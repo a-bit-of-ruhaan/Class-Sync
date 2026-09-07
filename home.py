5ff247159120ba35b4870ee23d47da3aba67dada
Ruhaan <ruhaankapoor07@gmail.com>
2026-09-07 11:24:10 +0530

    Add files via upload

--- home.py
+++ home.py
@@ -3,45 +3,42 @@ import streamlit as st
 st.set_page_config(
     layout="wide",
     page_title="classync",
+    initial_sidebar_state="collapsed"
 
 )
 
-st.title("ClassSync")
-st.write("your personal class data management system")
-st.sidebar.title("Navigation")
-st.sidebar.write("Hi user! Welcome to ClassSync. Please use the navigation bar to explore the app.")
+#injecting css
 
-st.sidebar.markdown("---")
-st.sidebar.button("try ai!")
+with open("styles/home.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-st.sidebar.markdown("---")
-st.sidebar.markdown("### navigation")
+#header of the page
 
-st.markdown('<div class="content_container">', unsafe_allow_html=True)
-st.markdown('<div class="content_box">', unsafe_allow_html=True)
-st.markdown('<h4>About ClassSync</h4>', unsafe_allow_html=True)
-st.markdown('<p>ClassSync is a comprehensive platform designed to help students manage their class data effectively. With features like note uploading, viewing, and summarization, ClassSync aims to streamline the learning process and enhance productivity.</p>', unsafe_allow_html=True)
-
+st.markdown('<div class="header">', unsafe_allow_html=True)
+st.markdown('<h1>ClassSync</h1>', unsafe_allow_html=True)
+st.markdown('<p>Welcome to ClassSync, your ultimate study companion!</p>', unsafe_allow_html=True)
 st.markdown('</div>', unsafe_allow_html=True)
 
+#sidebar configuration
 
-st.markdown('<div class="recents_container">', unsafe_allow_html=True)
 
+#main content
 
-col1, col2, col3 = st.columns(3)
+col1, col2, col3= st.columns([1,2,1])
 
 with col1:
     st.subheader("Summarizer")
+    st.image("images/summarize.png", width=400)
     st.write("Get your pdf, docx and other txt files summarized with our Smart Sum AI")
     if st.button("Get Summary of Your Files", use_container_width=True):
      st.switch_page("pages/summarizer.py")
 
-
-    with col2:
-        st.empty()
+with col2:
+   st.empty()
 
 with col3:
     st.subheader("View Notes")
+    st.image("images/note.png", width=400)
     st.write("Keep track of your previous notes and don't miss any.")
     if st.button("View Notes", use_container_width=True):
       st.switch_page("pages/notes.py")
@@ -52,6 +49,5 @@ st.markdown('<div class="content_container">', unsafe_allow_html=True)
 st.markdown('<div class="content_box">', unsafe_allow_html=True)
 st.markdown('<h2>DID YOU KNOW?</h2>', unsafe_allow_html=True)
 st.markdown('</div>', unsafe_allow_html=True)
-
 
 
